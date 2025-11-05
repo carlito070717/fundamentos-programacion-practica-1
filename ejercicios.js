457ee3fdbe8118529d5d2a0f95d4f808613a434d
Carlito070717 <carlos.llanes@tecdesoftware.edu.mx>
2025-11-05 08:36:33 -0600

    Ejercicio 8 resuelto

--- ejercicios.js
+++ ejercicios.js
@@ -121,8 +121,14 @@ function mayorDeTres(a, b, c) {
  * @returns {string} - "menor" (0-17), "adulto" (18-64), "mayor" (65+)
  */
 function clasificarEdad(edad) {
-  // TODO: Implementa la clasificaciÃ³n con if/else if/else
-  return "";
+  // TODO: Implementa la clasificación con if/else if/else
+  if (edad >= 0 && edad <= 17) {
+    return "menor";
+  } else if (edad >= 18 && edad <= 64) {
+    return "adulto";
+  } else {
+    return "mayor";
+  }
 }
 
 // ============================================
