cb7b2e224ec7c3008895387f779736a64e5f5175
Carlito070717 <carlos.llanes@tecdesoftware.edu.mx>
2025-11-05 11:20:01 -0600

    Ejercicio 19 resuelto

--- ejercicios.js
+++ ejercicios.js
@@ -319,9 +319,15 @@ function crearMatriz(filas, columnas) {
  * Ejemplo: sumaMatriz([[1, 2], [3, 4]]) debe retornar 10
  */
 function sumaMatriz(matriz) {
-  // TODO: Suma todos los elementos de la matriz
-  // NecesitarÃ¡s dos bucles: uno para las filas y otro para las columnas
-  return 0;
+  // Usé dos bucles for anidados para recorrer todas las filas y columnas
+  // y fui acumulando cada elemento en la variable suma
+  let suma = 0;
+  for (let i = 0; i < matriz.length; i++) {
+    for (let j = 0; j < matriz[i].length; j++) {
+      suma += matriz[i][j];
+    }
+  }
+  return suma;
 }
 
 /**
