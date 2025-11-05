6b9d61f281c45e1a7cc7a062aa0bd6a3bb1021b7
Carlito070717 <carlos.llanes@tecdesoftware.edu.mx>
2025-11-05 08:07:35 -0600

    Ejercicio 1 resuelto

--- ejercicios.js
+++ ejercicios.js
@@ -15,9 +15,9 @@ function miInformacion() {
   // TODO: Declara las variables nombre, edad y carrera
   // Ejemplo: const nombre = "Juan";
   
-  const nombre = "";
-  const edad = 0;
-  const carrera = "";
+  const nombre = "Carlos";
+  const edad = 18;
+  const carrera = "Ingeniería de Software";
   
   return { nombre, edad, carrera };
 }
